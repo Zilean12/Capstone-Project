afcc10155485ad3dcd2476ed0a3755065386844c
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-02-12 09:57:07 +0530

    Update index.js
    Signed-off-by: Aryan Sharma <aryan.sharma21@st.niituniversity.in>

--- signup/index.js
+++ signup/index.js
@@ -26,13 +26,13 @@ function signUp(){
     });
 }
 
-function forgotPass(){
-    const email = document.getElementById("email").value
-    firebase.auth().sendPasswordResetEmail(email)
-    .then(() => {
-        alert("Reset link sent to your email id")
-    })
-    .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
-    });
-}
+// function forgotPass(){
+//     const email = document.getElementById("email").value
+//     firebase.auth().sendPasswordResetEmail(email)
+//     .then(() => {
+//         alert("Reset link sent to your email id")
+//     })
+//     .catch((error) => {
+//         document.getElementById("error").innerHTML = error.message
+//     });
+// }
